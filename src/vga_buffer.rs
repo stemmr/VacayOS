7cf59b769a3ac0c3a29ac6a66d54dbed0292a1e7
apstemmer <apstemmer@hotmail.com>
2019-07-29 16:24:33 +0100

    immutably static

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,6 @@
 use volatile::Volatile;
 use core::fmt;
+use lazy_static::lazy_static;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -76,7 +77,16 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH{
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row -1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT-1);
+        self.column_position = 0;
+    }
 
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
@@ -87,6 +97,16 @@ impl Writer {
                 _ => self.write_byte(0xfe),
             }
 
+        }
+    }
+
+    fn clear_row(&mut self, row:usize){
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH{
+            self.buffer.chars[row][col].write(blank);
         }
     }
 }
@@ -109,4 +129,11 @@ pub fn print_something() {
     writer.write_byte(b'H');
     writer.write_string("ello ");
     write!(writer, "Using formatting macros as {} and {}", 42,1.0/2.7).unwrap();
+}
+lazy_static!{
+    pub static ref WRITER: Writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer)},
+    };
 }

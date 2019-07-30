dedb7776b80eec020edaf37fe325c07d24f9f410
apstemmer <apstemmer@hotmail.com>
2019-07-30 23:34:51 +0100

    printing onto console via serial

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,64 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+mod vga_buffer;
+mod serial;
 
 use core::panic::PanicInfo;
 
-mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello World!";
 
 //cargo xrun to compile kernal and boot into QEMU
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), "fmt macros as {} and {}", 42,1.0/2.7).unwrap();
+    println!("Hello World{}","!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    serial_print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    serial_println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
